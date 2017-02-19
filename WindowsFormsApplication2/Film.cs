4675e3689ce1c8b4e17edc151344fb5344981df5
thefurryman <tofumaster536@gmail.com>
2017-02-19 14:29:04 -0800

    initial
    - skeleton classes (no functionality)
    - some designs implemented

---
+++ WindowsFormsApplication2/Film.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPP.CS.CS408.FilmLib
+{
+    class Film
+    {
+        private string filmName;
+        private int rating;
+        private string[] tags;
+
+        public Film(string name, int rate, string[] tag)
+        {
+            filmName = name;
+            rating = rate;
+            tags = tag;
+        }
+
+        public string GetFilmName()
+        {
+            return filmName;
+        }
+
+        public int GetRating()
+        {
+            return rating;
+        }
+
+        public string[] GetTags()
+        {
+            return tags;
+        }
+    }
+}
---
+++ WindowsFormsApplication2/FilmDB.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CPP.CS.CS408.FilmLib
+{
+    class FilmDB
+    {
+        private List<Film> films;
+
+        public FilmDB()
+        {
+            films = new List<Film>();
+        }
+
+        public void Add(Film film)
+        {
+            films.Add(film);
+        }
+
+        public Film SearchName(string filmName) //might change to list of films with short search 
+        {
+            string nameTemp;
+            filmName = filmName.ToUpper();
+            foreach(Film film in films)
+            {
+                nameTemp = film.GetFilmName().ToUpper();
+                if (nameTemp.Equals(filmName))
+                {
+                    return film;
+                }
+            }
+            return null;
+        }
+    }
+}
